3d03989f8364dbab117baea3cdd37533029382b2
riddhiraj <87946275+RiddhiRaj@users.noreply.github.com>
2022-08-08 01:03:11 +0530

    .

---
+++ Assignment 1/array-2d-search.c
@@ -0,0 +1,11 @@
+//Write a C program to search an element in a 2D-Array using dynamic memory allocation.
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    printf("Enter the number of rows in the array");
+    int r;
+    scanf("%d",&r);
+    
+    return 0;
+}
