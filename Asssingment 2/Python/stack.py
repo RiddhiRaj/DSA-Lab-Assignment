94e6677a6d7f1125e227d74610115a585331aac5
riddhiraj <87946275+RiddhiRaj@users.noreply.github.com>
2022-08-16 13:44:28 +0530

    .

--- Asssingment 2/Python/stack.py
+++ Asssingment 2/Python/stack.py
@@ -1,15 +1,37 @@
 #write a python to implement a stack
-class Stack:
-    def __init__(self):
-        self.items = []
-    def isEmpty(self):
-        return self.items == []
-    def push(self, item):
-        self.items.append(item)
-    def pop(self):
-        return self.items.pop()
-    def peek(self):
-        return self.items[len(self.items)-1]
-    def size(self):
-        return len(self.items)
+
+stack = []
+
+def push(item):
+    stack.append(item)
+    return stack
+
+def pop():
+    if len(stack) == 0:
+        return "Stack is empty"
+    else:
+        return stack.pop()
+
+def peek():
+    if len(stack) == 0:
+        return "Stack is empty"
+    else:
+        return stack[-1]
+
+
+choice = int(input("1. Push\n2. Pop\n3. Peek\n4. Exit\nEnter your choice: "))
+while choice != 4:
+    if choice == 1:
+        item = input("Enter the item to push: ")
+        push(item)
+        print("Stack: ", stack)
+    elif choice == 2:
+        print("Popped item: ", pop())
+        print("Stack: ", stack)
+    elif choice == 3:
+        print("Peeked item: ", peek())
+        print("Stack: ", stack)
+    else:
+        print("Invalid choice")
+    choice = int(input("1. Push\n2. Pop\n3. Peek\n4. Exit\nEnter your choice: "))
 
