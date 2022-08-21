0aaddd9357ff9cbf2ceb7394b26639075278ebf6
riddhiraj <87946275+RiddhiRaj@users.noreply.github.com>
2022-08-22 03:47:10 +0530

    add comments

--- Assignment 3/Python/insert-linkedlist.py
+++ Assignment 3/Python/insert-linkedlist.py
@@ -23,28 +23,28 @@ class LinkedList:
             self.insertAtLast(data)
 
     def insertAtFirst(self, data):
-        newNode = Node(data)
+        newNode = Node(data) # create a new node
         newNode.next = self.head #here we are pointing the new node to the head of the list
         self.head = newNode #here we are making the new node as the head of the list
 
     def insertAtLast(self, data):
         newNode = Node(data)
-        if self.head is None:
+        if self.head is None: #if the list is empty then make the new node as the head of the list
             self.head = newNode
             return
-        last = self.head
-        while last.next:
-            last = last.next
-        last.next = newNode
-
+        last = self.head #make a pointer to the head of the list
+        while last.next: #loop until the last node of the list
+            last = last.next #move the pointer to the next node of the list
+        last.next = newNode #make the new node as the next node of the last node of the list
+ 
     def insertAtAny(self, data, x):
         newNode = Node(data)
-        p = self.head
+        p = self.head #make a pointer to the head of the list
         i = 0
-        while i < x-1 and p is not None:
-            p = p.next
-            i += 1
-        if p is None:
+        while i < x-1 and p is not None:  #loop until the x-1 node of the list
+            p = p.next #move the pointer to the next node of the list
+            i += 1 #increment the counter
+        if p is None: #if the counter reaches the end of the list then return that
             print("Index out of bound")
         else:
             newNode.next = p.next
@@ -55,10 +55,10 @@ class LinkedList:
             print("List is empty")
             return
         else:
-            p = self.head
-            while p:
-                print(p.data, " ", end='')
-                p = p.next
+            p = self.head #make a pointer to the head of the list
+            while p: #loop until the last node of the list
+                print(p.data, " ", end='') 
+                p = p.next #move the pointer to the next node of the list
             print()
 
 list = LinkedList()
